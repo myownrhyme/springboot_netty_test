2109869e03791c261a4dc0d6f46f504f40ac22d7
myownrhyme <lxyownrhyme@gmail.com>
2019-01-28 17:35:27 +0800

    servlet处理handler

--- src/main/java/com/lxy/webcontainer/App/core/NettyServletContainer.java
+++ src/main/java/com/lxy/webcontainer/App/core/NettyServletContainer.java
@@ -1,13 +1,55 @@
 package com.lxy.webcontainer.App.core;
 
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.ChannelOption;
+import io.netty.channel.ChannelPipeline;
+import io.netty.channel.EventLoopGroup;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+import io.netty.handler.codec.http.HttpServerCodec;
+import io.netty.util.concurrent.DefaultEventExecutorGroup;
 import org.springframework.boot.web.server.WebServer;
 import org.springframework.boot.web.server.WebServerException;
 
+import java.net.InetSocketAddress;
+
+import static jdk.internal.jline.internal.Preconditions.checkNotNull;
+
+
 public class NettyServletContainer implements WebServer {
 
+    private final InetSocketAddress address;
+    private final NettyServletContext context;
+    private EventLoopGroup bossGroup;
+    private EventLoopGroup workGroup;
+    private DefaultEventExecutorGroup dispatherExecutor;
+
+    public NettyServletContainer(InetSocketAddress address,NettyServletContext context){
+        this.address = address;
+        this.context = context;
+    }
     @Override
     public void start() throws WebServerException {
-
+        ServerBootstrap sb = new ServerBootstrap();
+        bossGroup = new NioEventLoopGroup(1);
+        workGroup = new NioEventLoopGroup();
+        dispatherExecutor = new DefaultEventExecutorGroup(50);
+        sb.channel(NioServerSocketChannel.class)
+                .group(bossGroup, workGroup);
+        sb.option(ChannelOption.TCP_NODELAY, true)
+                .option(ChannelOption.SO_REUSEADDR, true)
+                .option(ChannelOption.SO_BACKLOG, 100);
+        sb.childHandler(new ChannelInitializer<SocketChannel>() {
+            @Override
+            protected void initChannel(SocketChannel ch) throws Exception {
+                ChannelPipeline p = ch.pipeline();
+                p.addLast("codec", new HttpServerCodec(4096, 8192, 8192, false)); //HTTP编码解码Handler
+                p.addLast("servletInput", new NettyServletHandler(context)); //处理请求，读入数据，生成Request和Response对象
+                p.addLast(checkNotNull(dispatherExecutor), "filterChain", new RequestDispatcherHandler(context)); //获取请求分发器，让对应的Servlet处理请求，同时处理404情况
+            }
+        });
     }
 
     @Override
--- src/main/java/com/lxy/webcontainer/App/core/NettyServletHandler.java
+++ src/main/java/com/lxy/webcontainer/App/core/NettyServletHandler.java
@@ -3,4 +3,6 @@ package com.lxy.webcontainer.App.core;
 import io.netty.channel.ChannelInboundHandlerAdapter;
 
 public class NettyServletHandler extends ChannelInboundHandlerAdapter {
+
+
 }
